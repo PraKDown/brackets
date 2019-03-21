a839f9c59360a9f12b511eaba82690df92b666ec
Vyacheslav Tkachev <dragbarxd@gmail.com>
2019-03-21 14:30:31 +0300

    style: make code more readable

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 module.exports = function check(str, bracketsConfig) {
-  let result = false;
   let basicstr = [];
   let addstr = [];
   for (let i = 0; i < bracketsConfig.length; i++) {
@@ -15,19 +14,19 @@ module.exports = function check(str, bracketsConfig) {
   for (let i = 0; i < str.length; i++) {
     for (let k = 0; k < basicstr.length; k++) {
 			if (str[i] == basicstr[k] && k % 2 == 0){
-					arraystr.push(str[i]);
-          }
+        arraystr.push(str[i]);
+      }
       else  if (str[i] == basicstr[k] && k % 2 != 0) {
-        if(arraystr[arraystr.length-1] == basicstr[k-1]) arraystr.pop();
+        if (arraystr[arraystr.length-1] == basicstr[k-1]) arraystr.pop();
         else arraystr.push(str[i]);
 			} 
 		}
 		for (let l = 0; l < addstr.length; l++) {
-				if (str[i] == addstr[l]) {
-					if (arraystr[arraystr.length-1] != addstr[l]) arraystr.push(str[i]);
-					else arraystr.pop();
-					} 
-				}
+      if (str[i] == addstr[l]) {
+        if (arraystr[arraystr.length-1] != addstr[l]) arraystr.push(str[i]);
+        else arraystr.pop();
+        } 
+      }
     }
   if (arraystr.length != 0) return false;
 	else return true;
